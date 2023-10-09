639d5ca840effdd3fa835d2c3326544ddf595a0a
viadot <kizz4224@yahoo.com>
2023-10-10 05:30:20 +1030

    stuff

--- app.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    <R />
-  </div>
-</template>
-<script setup lang="ts">
-import R from "~/pages/R.vue";
-</script>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+  <template>
+
+    <div>
+      <h1> Hello!</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Accusantium ad beatae consequuntur eius fugiat laborum obcaecati quibusdam repellendus tempore!
+        Cupiditate dignissimos doloremque facere maxime praesentium qui repellendus. Dicta, error voluptatem.
+      </p>
+    </div>
+
+  </template>
+<style scoped>
+</style>
