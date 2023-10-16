4b3339cea0fbe26ea24c9aaed3116a07bce65c4e
viadot <kizz4224@yahoo.com>
2023-10-16 14:08:13 +1030

    forgot contact.vue transition

--- pages/contact.vue
+++ pages/contact.vue
@@ -15,41 +15,56 @@ import NavBar from "~/components/navbar.vue";
      <h1 class="text-2xl font-medium"> Contact me </h1>
    </div>
    <div class="m-6"/>
-   <div class="block">
-     <div class="flex justify-center">
-       <div class="w-[13.5em] text-center">
-         <div class="discord border border-neutral-700">
-           <h1 class="text-light bg-neutral-900"> Got Discord? </h1>
-           <div class="bg-neutral-800">
-             <a href="https://discord.com/users/202862911398346753">
-               <img src="/icons/icons8-discord.svg" class="m-auto h-[64px] w-[64px]"/>
-               <span class=""> .wafflerthesecond </span>
-             </a>
+   <transition name="fade" mode="out-in">
+     <div>
+       <div class="block">
+         <div class="flex justify-center">
+           <div class="w-[13.5em] text-center">
+             <div class="discord border border-neutral-700">
+               <h1 class="text-light bg-neutral-900"> Got Discord? </h1>
+               <div class="bg-neutral-800">
+                 <a href="https://discord.com/users/202862911398346753">
+                   <img src="/icons/icons8-discord.svg" class="m-auto h-[64px] w-[64px]"/>
+                   <span class=""> .wafflerthesecond </span>
+                 </a>
+               </div>
+             </div>
+           </div>
+         </div>
+       </div>
+       <div class="py-[1em]"></div>
+       <div class="block">
+         <div class="flex justify-center">
+           <div class="w-[13.5em] text-center">
+             <div class="gmail border border-neutral-700">
+               <h1 class="text-light text-center bg-neutral-900"> Email me if you want! </h1>
+               <div class="bg-neutral-800">
+                   <a href="mailto: mathewdev.53@gmail.com">
+                    <img src="/icons/icons8-gmail.svg" class="m-auto h-[64px] w-[64px]"/>
+                    <span class=""> mathewdev.53@gmail.com </span>
+                   </a>
+               </div>
+             </div>
            </div>
          </div>
        </div>
      </div>
-   </div>
-   <div class="py-[1em]"></div>
-   <div class="block">
-     <div class="flex justify-center">
-       <div class="w-[13.5em] text-center">
-         <div class="gmail border border-neutral-700">
-           <h1 class="text-light text-center bg-neutral-900"> Email me if you want! </h1>
-           <div class="bg-neutral-800">
-               <a href="mailto: mathewdev.53@gmail.com">
-                <img src="/icons/icons8-gmail.svg" class="m-auto h-[64px] w-[64px]"/>
-                <span class=""> mathewdev.53@gmail.com </span>
-               </a>
-           </div>
-         </div>
-       </div>
-     </div>
-   </div>
+   </transition>
  </div>
 </template>
 
 <style scoped>
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 250ms ease-in;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
 .gmail:not(:hover){
   fill: #ffffff;
   opacity: 50%;
