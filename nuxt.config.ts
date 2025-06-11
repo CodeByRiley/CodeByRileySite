c29b07fd719a1e8f78f594a73e1b5aff5fc53320
Riley <mathewdev.53@gmail.com>
2025-06-11 19:28:01 +0930

    Add Vercel Analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,14 +2,14 @@
 import { defineNuxtConfig } from 'nuxt/config'
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  modules: ['@nuxt/ui'],
+  modules: ['@nuxt/ui', 'nuxt-vercel-analytics'],
   ssr: false,
   app: {
     head:{
       title: "CodeByRiley",
       meta: [
         { name: 'description', content: 'CodeByRiley is a website for Riley, a 20-year-old self-taught software developer from Australia.' },
-        { name: 'keywords', content: 'Riley, Software Developer, Code, RileyDev, RileyDevs, RileyDevs.com' },
+        { name: 'keywords', content: 'Riley, Software Developer,' },
         { name: 'author', content: 'RileyDev' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
         { charset: 'utf-8' }
